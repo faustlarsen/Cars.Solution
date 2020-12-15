973da718460bef927987f4655f2c166e3f8552ad
Constantine <faustlarsen@gmail.com>
2020-12-15 14:07:30 -0700

    add rest of files
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

---
+++ CarDealership/Models/Cars.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+namespace CarDealership.Models
+{
+  public class Car
+  {
+    public string MakeModel { get; set; }
+    public int Price { get; set; }
+    public int Miles  { get; set; }
+    
+    private static List<Car> _instances = new List<Car>{};
+
+    public Car(string makeModel, int price, int miles)
+    {
+      MakeModel = makeModel;
+      Price = price;
+      Miles = miles;
+      _instances.Add(this);
+    }
+
+    public static List<Car> GetAll()
+    {
+      return _instances;
+    }
+  }
+}
